fded56062f61e9feba35ef09f8851927d2de327c
aryanrsuri <13823329+aryanrsuri@users.noreply.github.com>
2023-01-23 19:30:17 -0800

    0.1.0 init

--- server.ts
+++ server.ts
@@ -1,7 +1,10 @@
 import { serve } from "aleph/solid-server";
 import routes from "./routes/_export.ts";
-
+import presetUno from "https://esm.sh/@unocss/preset-uno";
 serve({
   router: { routes },
   ssr: true,
+  unocss: {
+    presets: [presetUno()],
+  },
 });
